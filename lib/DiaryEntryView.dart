b1d006318298d38b1555367930213c406834041c
dius00 <antonio.derinaldis@outlook.com>
2020-11-25 16:36:21 +0900

    updated diary entry view, still WIP

--- lib/DiaryEntryView.dart
+++ lib/DiaryEntryView.dart
@@ -12,7 +12,50 @@ class _DiaryEntryViewState extends State<DiaryEntryView> {
   final int _numPages = 3;
   final PageController _pageController = PageController(initialPage: 0);
   int _currentPage = 0;
-/*
+  String textContent = "";
+  String initialText = "Write your entry";
+  bool _isEditingText = false;
+  TextEditingController _editingController;
+
+  @override
+  void initState() {
+    super.initState();
+    _editingController = TextEditingController(text: textContent);
+  }
+
+  @override
+  void dispose() {
+    _editingController.dispose();
+    super.dispose();
+  }
+
+  Widget _editTextField() {
+    if (_isEditingText)
+      return TextField(
+        onChanged: (newValue) {
+          setState(() {
+            textContent = newValue;
+          });
+        },
+        // decoration: InputDecoration(
+        //   border: InputBorder.none,
+        //   hintText: 'Write your entry',
+        //   // color: Colors.white,
+        //   // fontSize: 24.0,
+        // ),
+        autofocus: true,
+        controller: _editingController,
+      );
+    return Text(
+          initialText,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22.0,
+          ),
+        );
+  }
+
+  /*
   List<Widget> _buildPageIndicator() {
     List<Widget> list = [];
     for (int i = 0; i < _numPages; i++) {
@@ -20,6 +63,7 @@ class _DiaryEntryViewState extends State<DiaryEntryView> {
     }
     return list;
   }
+
 */
   Widget _indicator(bool isActive) {
     return AnimatedContainer(
@@ -28,7 +72,7 @@ class _DiaryEntryViewState extends State<DiaryEntryView> {
       height: 5.0,
       width: isActive ? 24.0 : 16.0,
       decoration: BoxDecoration(
-        color: isActive ? Color(0xFFFB8986) : Colors.white,
+        color: /*isActive ? Color(0xFFFB8986) :*/ Colors.white,
         borderRadius: BorderRadius.all(Radius.circular(12)),
       ),
     );
@@ -42,20 +86,20 @@ class _DiaryEntryViewState extends State<DiaryEntryView> {
         fontFamily: "Sofia",
         fontWeight: FontWeight.w600,
         fontSize: 23.0,
-        color: Colors.white);
+        color: Colors.white); // h1 text color
 
     var _textH2 = TextStyle(
         fontFamily: "Sofia",
         fontWeight: FontWeight.w200,
         fontSize: 16.0,
-        color: Colors.white);
+        color: Colors.white); // h2 text color
 
     return Scaffold(
       body: AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle.light,
         child: Container(
           decoration: BoxDecoration(
-            color: Color(0xFF1E2026),
+            color: Colors.blueGrey, // background color
           ),
           child: Stack(
             children: <Widget>[
@@ -80,8 +124,12 @@ class _DiaryEntryViewState extends State<DiaryEntryView> {
                           fit: BoxFit.cover,
                         ),
                         Container(
+                          //color: Colors.blueGrey,
                           margin:
-                              EdgeInsets.only(top: 0.0, bottom: _height / 3.8),
+                              EdgeInsets.only(top: 0.0, bottom: _height / 2.25),
+
+                          /*
+                          // uncomment to fade bottom part of picture
                           decoration: BoxDecoration(
                             gradient: LinearGradient(
                               begin: FractionalOffset(0.0, 0.0),
@@ -95,11 +143,12 @@ class _DiaryEntryViewState extends State<DiaryEntryView> {
                               ],
                             ),
                           ),
+                          */
                         ),
                         Align(
                           alignment: FractionalOffset.center,
                           child: Padding(
-                            padding: const EdgeInsets.only(top: 200.0),
+                            padding: const EdgeInsets.only(top: 25.0),
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               crossAxisAlignment: CrossAxisAlignment.center,
@@ -112,11 +161,22 @@ class _DiaryEntryViewState extends State<DiaryEntryView> {
                                 Padding(
                                   padding: const EdgeInsets.only(
                                       left: 15.0, right: 15.0),
-                                  child: Text(
-                                    'Entry State will live here, make this editable',
-                                    textAlign: TextAlign.center,
-                                    style: _textH2,
-                                  ),
+                                  child: _editTextField(),
+                                  // TextField(
+                                  //   onChanged: (text) {
+                                  //     print("First text field: $text");
+                                  //     setState(() {
+                                  //       textContent = text;
+                                  //     });
+                                  //   },
+                                  //   // decoration: InputDecoration(
+                                  //   //   border: InputBorder.none,
+                                  //   //   hintText: 'Write your entry',
+                                  //   //   // 'Entry State will live here, make this editable',
+                                  //   //   // textAlign: TextAlign.center,
+                                  //   //   // style: _textH2,
+                                  //   // ),
+                                  // ),
                                 ),
                               ],
                             ),
@@ -124,128 +184,18 @@ class _DiaryEntryViewState extends State<DiaryEntryView> {
                         ),
                       ],
                     ),
-                    /*
-                    Stack(
-                      children: <Widget>[
-                        Image(
-                          image: AssetImage(
-                              'assets/Template1/image/onBoarding2.jpeg'),
-                          height: 400.0,
-                          width: double.infinity,
-                          fit: BoxFit.cover,
-                        ),
-                        Container(
-                          margin:
-                              EdgeInsets.only(top: 0.0, bottom: _height / 3.8),
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              begin: FractionalOffset(0.0, 0.0),
-                              end: FractionalOffset(0.0, 1.0),
-                              // stops: [0.0, 1.0],
-                              colors: <Color>[
-                                Color(0xFF1E2026).withOpacity(0.1),
-                                Color(0xFF1E2026).withOpacity(0.3),
-                                Color(0xFF1E2026),
-                                Color(0xFF1E2026),
-                              ],
-                            ),
-                          ),
-                        ),
-                        Align(
-                          alignment: FractionalOffset.center,
-                          child: Padding(
-                            padding: const EdgeInsets.only(top: 245.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: <Widget>[
-                                Text(
-                                  'Live your life smarter',
-                                  style: _textH1,
-                                ),
-                                SizedBox(height: 35.0),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 15.0, right: 15.0),
-                                  child: Text(
-                                    'Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
-                                    textAlign: TextAlign.center,
-                                    style: _textH2,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Stack(
-                      children: <Widget>[
-                        Image(
-                          image: AssetImage(
-                              'assets/Template1/image/onBoarding3.jpeg'),
-                          height: 400.0,
-                          width: double.infinity,
-                          fit: BoxFit.cover,
-                        ),
-                        Container(
-                          margin:
-                              EdgeInsets.only(top: 0.0, bottom: _height / 3.8),
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              begin: FractionalOffset(0.0, 0.0),
-                              end: FractionalOffset(0.0, 1.0),
-                              // stops: [0.0, 1.0],
-                              colors: <Color>[
-                                Color(0xFF1E2026).withOpacity(0.1),
-                                Color(0xFF1E2026).withOpacity(0.3),
-                                Color(0xFF1E2026),
-                                Color(0xFF1E2026),
-                              ],
-                            ),
-                          ),
-                        ),
-                        Align(
-                          alignment: FractionalOffset.center,
-                          child: Padding(
-                            padding: const EdgeInsets.only(top: 245.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: <Widget>[
-                                Text(
-                                  'Live your life smarter',
-                                  style: _textH1,
-                                ),
-                                SizedBox(height: 35.0),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 15.0, right: 15.0),
-                                  child: Text(
-                                    'Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
-                                    textAlign: TextAlign.center,
-                                    style: _textH2,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  */
                   ],
                 ),
               ),
-              /*
+
               Align(
                 alignment: FractionalOffset.center,
                 child: Padding(
-                  padding: const EdgeInsets.only(top: 470.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: _buildPageIndicator(),
-                  ),
+                  padding: const EdgeInsets.only(top: 270.0),
+                  //child: Row(
+                  //mainAxisAlignment: MainAxisAlignment.center,
+                  //children: _buildPageIndicator(),
+                  //),
                 ),
               ),
               _currentPage != _numPages - 1
@@ -253,10 +203,17 @@ class _DiaryEntryViewState extends State<DiaryEntryView> {
                       alignment: FractionalOffset.bottomRight,
                       child: FlatButton(
                           onPressed: () {
-                            _pageController.nextPage(
-                              duration: Duration(milliseconds: 500),
-                              curve: Curves.ease,
-                            );
+                            setState(() {
+                              if(_isEditingText) {
+                                _isEditingText = false;
+                              } else {
+                                _isEditingText = true;
+                              }
+                            });
+                            // _pageController.nextPage(
+                            //   duration: Duration(milliseconds: 500),
+                            //   curve: Curves.ease,
+                            // );
                           },
                           child: Padding(
                             padding: const EdgeInsets.only(bottom: 20.0),
@@ -264,11 +221,15 @@ class _DiaryEntryViewState extends State<DiaryEntryView> {
                               height: 50.0,
                               decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(30.0),
-                                  color: Colors.transparent,
-                                  border: Border.all(color: Color(0xFFFB8986))),
+                                  color: Colors
+                                      .transparent, // background button color
+                                  border: Border.all(
+                                      color: Color(
+                                          0xFFFB8986)) // all border colors
+                                  ),
                               child: Center(
                                   child: Text(
-                                "Continue",
+                                "Edit/Save (make this smaller)",
                                 style: TextStyle(
                                     color: Color(0xFFFB8986),
                                     fontSize: 17.0,
@@ -280,45 +241,11 @@ class _DiaryEntryViewState extends State<DiaryEntryView> {
                           )),
                     )
                   : Text(''),
-                  */
+              //*/
             ],
           ),
         ),
       ),
-      /*
-      bottomSheet: _currentPage == _numPages - 1
-          ? InkWell(
-              onTap: () {
-                // Navigator.of(context).pushReplacement(PageRouteBuilder(
-                // pageBuilder: (_, __, ___) => new chooseLogin()));
-              },
-              child: Container(
-                height: 60.0,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: <Color>[
-                      Color(0xFFFEE140),
-                      Color(0xFFFA709A),
-                    ],
-                  ),
-                ),
-                child: Center(
-                  child: Text(
-                    'Get started',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 19.0,
-                      letterSpacing: 1.2,
-                      fontWeight: FontWeight.w500,
-                      fontFamily: "Poppins",
-                    ),
-                  ),
-                ),
-              ),
-            )
-          : Text(''),
-          */
     );
   }
 }
