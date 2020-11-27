78025cd09a795ec014199a3df47a1e981fbabdd5
Jules Montojo <jay@Juless-MacBook-Pro.local>
2020-11-27 10:37:59 +0900

    FLNP initialization

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,39 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'views/LoginPage.dart';
 import 'manager/Firebase.dart';
-import 'Navigation.dart';
+import 'Navigation.dart'; 
+
+//For Flutter__Local_Notifications_plugin
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+// import 'package:timezone/data/latest.dart' as tz;
+// import 'package:timezone/timezone.dart' as tz;
+
+//initializing the plugin
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+//initialize settings for Flutter__Local_Notifications_plugin
+  var initializationSettingsAndroid =
+      AndroidInitializationSettings('defaultIcon');
+  var initializationSettingsIOS = IOSInitializationSettings(
+    requestAlertPermission: true,
+    requestBadgePermission: true,
+    requestSoundPermission: true,
+    onDidReceiveLocalNotification: 
+        (int id, String title, String body, String payload) async {});
+  var initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid, 
+    iOS: initializationSettingsIOS);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification: (String payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: ' + payload);
+    }
+  });
+//--End--//
+
   await initializeFirebase();
   await setUpNotifications();
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
